bc834f253f4e8c1b87bb81b7e7fa709fcfe4dc30
asifwebsoft <mdasif2100@gmail.com>
2026-05-09 14:39:17 +0530

    fixed cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -49,6 +49,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://team-crm-iota.vercel.app",
+]
+
 CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'backend.urls'
