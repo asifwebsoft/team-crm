e6067802143d14abab500ae677dff87abbb941f9
asifwebsoft <111778350+asifwebsoft@users.noreply.github.com>
2026-05-09 16:20:56 +0530

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,6 +4,14 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+    <div>
+      <h1>CRM Working Successfully</h1>
+    </div>
+  )
+}
+
+export default function Home() {
+  return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
