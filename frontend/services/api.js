4c1d6c64ff6ebcf8d1ee3818c61983169c139a40
asifwebsoft <mdasif2100@gmail.com>
2026-05-10 17:52:40 +0530

    fixe router

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,24 +1,25 @@
-
 import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://team-crm-backend.onrender.com/api",
 });
 
-// Request interceptor
-API.interceptors.request.use(
-  (req) => {
+API.interceptors.request.use((req) => {
+
+  const publicRoutes = [
+    "/accounts/admin-signup/",
+    "/accounts/login/",
+  ];
+
+  if (!publicRoutes.includes(req.url)) {
     const token = localStorage.getItem("access");
 
     if (token) {
       req.headers.Authorization = `Bearer ${token}`;
     }
-
-    return req;
-  },
-  (error) => {
-    return Promise.reject(error);
   }
-);
+
+  return req;
+});
 
 export default API;
