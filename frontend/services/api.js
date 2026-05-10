0de6ad8f041b1232626c29f16e8fa63d5c0286f4
asifwebsoft <mdasif2100@gmail.com>
2026-05-10 15:39:02 +0530

    fixed api page

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -4,54 +4,14 @@ const API = axios.create({
   baseURL: "https://team-crm-backend.onrender.com/api",
 });
 
-// 🔥 REQUEST INTERCEPTOR (AUTO TOKEN)
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access");
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
 
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    req.headers.Authorization = `Bearer ${token}`;
   }
 
-  return config;
+  return req;
 });
-
-// 🔥 RESPONSE INTERCEPTOR (AUTO REFRESH TOKEN)
-API.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-
-    // अगर token expire हो गया
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-
-      const refresh = localStorage.getItem("refresh");
-
-      if (!refresh) {
-        window.location.href = "/login";
-        return Promise.reject(error);
-      }
-
-      try {
-        const res = await axios.post(
-          "http://127.0.0.1:8000/api/token/refresh/",
-          { refresh }
-        );
-
-        const newAccess = res.data.access;
-        localStorage.setItem("access", newAccess);
-
-        originalRequest.headers.Authorization = `Bearer ${newAccess}`;
-        return API(originalRequest);
-
-      } catch (err) {
-        localStorage.clear();
-        window.location.href = "/login";
-      }
-    }
-
-    return Promise.reject(error);
-  }
-);
 
 export default API;
