38923e08f0994a90ba31d72c7c1caed3af9f5e9c
asifwebsoft <mdasif2100@gmail.com>
2026-05-11 00:11:36 +0530

    dashboard fix

--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -24,7 +24,7 @@ export default function Dashboard() {
     if (typeof window !== "undefined") {
 
       const token =
-        localStorage.getItem("token");
+        localStorage.getItem("access");
 
       const userRole =
         localStorage.getItem("role");
