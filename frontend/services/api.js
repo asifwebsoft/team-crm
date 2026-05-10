c840defa70598b30537985bccdee43e9aeb230d5
asifwebsoft <mdasif2100@gmail.com>
2026-05-10 15:50:40 +0530

    fixed api page

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -5,7 +5,7 @@ const API = axios.create({
 });
 
 API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("access");
 
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
