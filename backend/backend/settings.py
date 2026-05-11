8b9fac82c447e54b3e399ca066ccaa69336e8874
asifwebsoft <mdasif2100@gmail.com>
2026-05-11 10:16:32 +0530

    create order fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from datetime import timedelta
 import dj_database_url 
 from decouple import config
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
