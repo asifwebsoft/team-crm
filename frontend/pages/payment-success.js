85b300b7a582aeab8069bff41bd064f5ba144025
asifwebsoft <mdasif2100@gmail.com>
2026-05-14 00:14:51 +0530

    final subscription verify

--- frontend/pages/payment-success.js
+++ frontend/pages/payment-success.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import axios from "axios";
 
 export default function PaymentSuccess() {
 
@@ -7,17 +8,41 @@ export default function PaymentSuccess() {
 
   useEffect(() => {
 
-    setTimeout(() => {
+    const verifySubscription = async () => {
 
-      router.push("/company");
+      try {
 
-    }, 2000);
+        const token = localStorage.getItem("access");
+
+        await axios.post(
+          "https://team-crm-backend.onrender.com/api/subscription/verify/",
+          {
+            plan: "pro"
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        router.push("/company");
+
+      } catch (err) {
+
+        console.log("VERIFY ERROR:", err);
+
+        router.push("/subscription");
+      }
+    };
+
+    verifySubscription();
 
   }, []);
 
   return (
     <div>
-      <h1>Payment Successful</h1>
+      <h1>Payment Successful...</h1>
     </div>
   );
 }
