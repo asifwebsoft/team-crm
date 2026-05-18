58e9e814fd3844455016e83d8b369b6c4a00e3f4
asifwebsoft <mdasif2100@gmail.com>
2026-05-18 16:46:14 +0530

    fix cust name in history view

--- backend/leads/views.py
+++ backend/leads/views.py
@@ -598,7 +598,7 @@ class LeadHistoryView(APIView):
                 "id": h.id,
 
                 "lead_name":
-                    h.lead.customer_name,
+                    h.customer_name,
 
                 "phone":
                     h.phone,
