62c4d9b97b144f64d71dec5524a670327f8edd4f
asifwebsoft <mdasif2100@gmail.com>
2026-05-21 15:34:31 +0530

    fixed smtp email backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
