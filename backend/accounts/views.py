b4904e57d65b1d5617305b1a0db64d04e3d7f87f
asifwebsoft <mdasif2100@gmail.com>
2026-05-21 16:49:55 +0530

    fixed reset

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -434,7 +434,8 @@ class ForgotPasswordView(APIView):
             reset_link = (
 
                 reset_link = (
-                    f"https://team-crm-roan.vercel.app"f"/reset-password/{uid}/{token}"
+                    f"https://team-crm-roan.vercel.app"
+                    f"/reset-password/{uid}/{token}"
                 )
             )
 
