93822945d51081310f247fd7f03c1f9510718abe
asifwebsoft <mdasif2100@gmail.com>
2026-05-24 15:36:22 +0530

    inventry urls fixed

--- backend/inventory/urls.py
+++ backend/inventory/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
-from .views import InventoryListCreateView, PurchaseEntryView
-
+from .views import (
+    InventoryListCreateView,
+    PurchaseEntryView
+)
 
 urlpatterns = [
+
     path(
         "",
         InventoryListCreateView.as_view()
     ),
 
     path(
-    "purchase-entry/",
-    PurchaseEntryView.as_view()
-),
+        "purchase-entry/",
+        PurchaseEntryView.as_view()
+    ),
 ]
